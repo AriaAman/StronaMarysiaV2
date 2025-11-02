29a2a482df7849f7a9134abc089bad70a4e1ad78
AriaAman <ariaaman@outlook.fr>
2025-11-02 23:09:44 +0100

    feat: migration vers calendrier FELG externe - suppression API interne reservation

--- plugins/fullcalendar.client.js
+++
@@ -1,8 +0,0 @@
-// Plugin pour configurer FullCalendar
-export default defineNuxtPlugin(() => {
-  // Configuration globale de FullCalendar si nécessaire
-  if (process.client) {
-    // Code côté client uniquement
-    console.log('FullCalendar plugin loaded')
-  }
-})
